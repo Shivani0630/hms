532ee016c7da676d89e24d44ac53fed843057c57
user <user@DESKTOP-FCLNE8N>
2023-08-16 17:15:50 +0530

    Framework

---
+++ src/test/java/CommandExcn/DocterLogin.java
@@ -0,0 +1,18 @@
+package CommandExcn;
+
+import org.testng.annotations.Test;
+
+public class DocterLogin {
+	@Test(groups = "Smoke")
+	public void DoctorRigister()
+	{
+		System.out.println("----succesfull(2)-------");
+	}
+	@Test(groups = "regression")
+	public void DoctorLogin()
+	{
+		System.out.println("----succesfully Loggedin to DoctorLogin-------");
+	}
+
+
+}
---
+++ src/test/java/CommandExcn/UserRegistration.java
@@ -0,0 +1,17 @@
+package CommandExcn;
+
+import org.testng.annotations.Test;
+
+public class UserRegistration {
+	@Test(groups = "Smoke")
+	public void UserRigister()
+	{
+		System.out.println("----succesfull(1)-------");
+	}
+	@Test(groups = "regression")
+	public void UserLogin()
+	{
+		System.out.println("----succesfully Loggedin to UserLogin-------");
+	}
+
+}
