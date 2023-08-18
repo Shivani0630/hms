966c6a01cb62b1e6ba1c6911863cfd629bc171d0
shivani0630 <shivuishu0630@gmail.com>
2023-08-18 09:48:57 +0530

    changes

---
+++ src/test/java/CommandExcn/pullrequset.java
@@ -0,0 +1,35 @@
+package CommandExcn;
+
+import org.testng.annotations.Test;
+
+public class pullrequset {
+	@Test(groups = "Smoke")
+	public void UserRigister()
+	{
+		System.out.println("----succesfull(1)-------");
+	}
+	@Test(groups = "regression")
+	public void UserLogin()
+	{
+		System.out.println("----succesfully Loggedin to UserLogin-------");
+	}
+	@Test(groups = "regression")
+	public void UserLogout()
+	{
+		System.out.println("----succesfully Loggedin to UserLogin-------");
+	}
+	@Test(groups = "regression")
+	public void UserLogged()
+	{
+		System.out.println("----succesfully Loggedin to UserLogin-------");
+	}
+	@Test(groups = "regression")
+	public void User()
+	{
+		System.out.println("----succesfully Loggedin to UserLogin-------");
+	}
+
+
+
+
+}
